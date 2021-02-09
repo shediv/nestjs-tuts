a8675f4a58ee3f9734ab30f15a3f7ad2e5138782
videsh_hedge <videsh_hedge@persistent.com>
2021-02-09 17:33:51 +0530

    Added basic validation

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,11 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateTaskDto {
     id: string;
+
+    @IsNotEmpty()
     title: string;
+
+    @IsNotEmpty()
     description: string;
 }
